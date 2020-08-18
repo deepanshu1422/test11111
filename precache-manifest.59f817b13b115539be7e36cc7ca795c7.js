78f3478e16dcbf0f3d0770db88797a662cc58f53
Deepanshu Udhwani <deepanshu1422@gmail.com>
2020-08-19 00:22:34 +0530

    Add files via upload

---
+++ precache-manifest.59f817b13b115539be7e36cc7ca795c7.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "480e85f1d70bfb63bf7464902a1f2ace",
+    "url": "/developerFolio/index.html"
+  },
+  {
+    "revision": "ac80127f8e3cae9188c7",
+    "url": "/developerFolio/static/css/3.70d62d19.chunk.css"
+  },
+  {
+    "revision": "62088ff0c40760dbcc9e",
+    "url": "/developerFolio/static/css/4.01c87591.chunk.css"
+  },
+  {
+    "revision": "7de19643cff5d47dc190",
+    "url": "/developerFolio/static/css/main.b7330e1a.chunk.css"
+  },
+  {
+    "revision": "a5232ac04ece86783ea3",
+    "url": "/developerFolio/static/js/2.3078cb03.chunk.js"
+  },
+  {
+    "revision": "ac80127f8e3cae9188c7",
+    "url": "/developerFolio/static/js/3.a1c6f930.chunk.js"
+  },
+  {
+    "revision": "62088ff0c40760dbcc9e",
+    "url": "/developerFolio/static/js/4.d2340b2c.chunk.js"
+  },
+  {
+    "revision": "7de19643cff5d47dc190",
+    "url": "/developerFolio/static/js/main.95974259.chunk.js"
+  },
+  {
+    "revision": "ccf182fa36a8371dfac7",
+    "url": "/developerFolio/static/js/runtime-main.c7bb0ca1.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/developerFolio/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/developerFolio/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "68e841388612be0c5143e2932fb77d4f",
+    "url": "/developerFolio/static/media/contactMail.68e84138.webp"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/developerFolio/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "c050c44b9bd79e3ecb565142c803ccab",
+    "url": "/developerFolio/static/media/facebookLogo.c050c44b.png"
+  },
+  {
+    "revision": "e79c639294c805688be731921368c8f8",
+    "url": "/developerFolio/static/media/googleAssistantLogo.e79c6392.webp"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/developerFolio/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/developerFolio/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "ee62cb58630f29a6e6201b49d4c03728",
+    "url": "/developerFolio/static/media/pwaLogo.ee62cb58.webp"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/developerFolio/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "de3da8064f2e9ffab945772c2c67e46f",
+    "url": "/developerFolio/static/media/talksCardBack.de3da806.svg"
+  }
+]);
